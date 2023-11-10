7e3f7aadb11e2f0e553999cd6505d31e877fcffb
mrdil <mrdil@BMW>
2023-11-10 21:45:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module8_dz1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            indexator indexator1 = new indexator(5);
+
+            // Устанавливаем значения через индексатор
+            indexator1[0] = 2;
+            indexator1[1] = 3;
+            indexator1[2] = 4;
+            indexator1[3] = 5;
+            indexator1[4] = 6;
+
+            // Получаем значения через индексатор и выводим их
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"indexator1[{i}] = {indexator1[i]}");
+            }
+        }
+    }
+}
---
+++ indexator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module8_dz1
+{
+    class indexator
+    {
+        private int[] data;
+
+        public indexator(int size)
+        {
+            data = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < data.Length)
+                {
+                    // Возвращаем текущее значение элемента массива
+                    return data[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < data.Length)
+                {
+                    // Возводим передаваемое значение в квадрат и устанавливаем его
+                    data[index] = value * value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+    }
+}
